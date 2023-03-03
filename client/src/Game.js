e7af5ae70fb59f8e093dbe1a1a049af37fc0ec70
charvar16 <proton74955ilo@protonmail.com>
2023-03-03 14:54:49 -0400

    testing useState for room data

--- client/src/Game.js
+++ client/src/Game.js
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import TicTacToe from './TicTacToe'
 import "./TicTacToe.css";
 
-const GameTable = ({exitGame}) => 
+const GameTable = ({exitGame,roomTest}) => 
 {
 return (
     <div>
-        <h1>Welcome to your game</h1>
+        <h1>Welcome to your game: {roomTest}</h1>
         <TicTacToe />
-        <p>Players will TOTALLY arrive shortly</p>
+        <p>Players will arrive shortly</p>
         <Link to={`/`} onClick={()=>exitGame()}>Salir del Juego</Link>
     </div>
   )} 
